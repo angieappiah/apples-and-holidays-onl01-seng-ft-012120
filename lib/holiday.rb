a9b657d523a5956bd13c633abeed0bd1a7387598
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 03:30:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,9 +19,11 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 end
 
-def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
+def
+second_supply_for_fourth_of_july (holiday_supplies)
+holiday_supplies = {:summer => "fireworks", "BBQ", :Christmas => " Lights", "Wreath", :New_year => "party hats"}
+end
+  
   # Christmas AND the New Year's arrays
 
 end
